1e23d1b9e8bf00780ee3355b018813dd623eed87
Jack <emaja@gmail.com>
2020-04-12 17:44:23 +0200

    working linked list

--- mycode.C
+++ mycode.C
@@ -1,8 +1,16 @@
-#include <stdio.h>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-int main()
+namespace Rextester
 {
-    printf("Hello, World!\n");
-
-    return 0;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+        }
+    }
 }
