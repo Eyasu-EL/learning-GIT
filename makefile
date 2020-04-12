da8be820e4f9729d97819c07f6e7c95066aa15fb
Jack <emaja@gmail.com>
2020-04-12 19:09:04 +0200

    makefile in the newlink branch

---
+++ makefile
@@ -0,0 +1,7 @@
+Console.WriteLine("I am programmer");
+            string prog = "I am Programmer";
+            for(int i = 0; i < 5; i++)
+            {
+                prog[i];     
+            }
+            Console.WriteLine(prog);
--- mycode.C
+++ mycode.C
@@ -11,6 +11,13 @@ namespace Rextester
         {
             //Your code goes here
             Console.WriteLine("Hello, world!");
+            Console.WriteLine("I am programmer");
+            string prog = "I am Programmer";
+            for(int i = 0; i < 5; i++)
+            {
+                prog[i];     
+            }
+            Console.WriteLine(prog);
         }
     }
 }
